fb9c3c0ed8b1b0b33f0931e92f4c77fe71d157be
Daniel Gasienica <daniel@signal.org>
2018-04-12 20:18:04 -0400

    Watch TypeScript files in Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -191,7 +191,7 @@ module.exports = function(grunt) {
         tasks: ['jscs']
       },
       transpile: {
-        files: ['./ts/**/*.js'],
+        files: ['./ts/**/*.ts'],
         tasks: ['exec:transpile']
       }
     },

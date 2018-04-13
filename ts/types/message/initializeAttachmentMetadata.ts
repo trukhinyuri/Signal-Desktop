b785d8e2aa73c2fe9ec629ebc49e444002d667ae
Daniel Gasienica <daniel@signal.org>
2018-04-13 16:27:38 -0400

    Skip metadata initialization for `verified-change` messages

--- ts/types/message/initializeAttachmentMetadata.ts
+++ ts/types/message/initializeAttachmentMetadata.ts
@@ -9,6 +9,9 @@ import { Message } from '../Message';
 export const initializeAttachmentMetadata = async (
   message: Message
 ): Promise<Message> => {
+  if (message.type === 'verified-change') {
+    return message;
+  }
 
   const numAttachments = message.attachments.length;
   const [numVisualMediaAttachments, numFileAttachments] = partition(
